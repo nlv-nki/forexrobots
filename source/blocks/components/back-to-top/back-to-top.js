3c5f60494ba04bf358354f904908887bbca0a7b7
Nikolai <nlv.nki@gmail.com>
2021-05-22 03:06:46 +0300

    refactoring code functions

--- source/blocks/components/back-to-top/back-to-top.js
+++ source/blocks/components/back-to-top/back-to-top.js
@@ -3,12 +3,18 @@ const back_to_top_btn = document.querySelector('.back-to-top');
 const page_menu_pos = page_menu.offsetTop;
 const page_menu_outer_pos = page_menu_pos + page_menu.clientHeight;
 const scrollWindow = () => {
-  if (window.pageYOffset > page_menu_outer_pos && !back_to_top_btn.classList.contains('back-to-top-visible')) {
-    back_to_top_btn.classList.add('back-to-top-visible');
-  }
-  else {
-    back_to_top_btn.classList.remove('back-to-top-visible');
-  }
+
+    setTimeout(() => {
+      if (window.pageYOffset > page_menu_outer_pos) {
+        back_to_top_btn.classList.add('back-to-top-visible');
+      }
+      else {
+        back_to_top_btn.classList.remove('back-to-top-visible');
+        console.log(window.pageYOffset,page_menu_outer_pos  );
+        console.log('del class');
+      }
+    },500)
+
 }
 const backToTop = () => {
   window.scrollTo({top:0, behavior: "smooth"})
